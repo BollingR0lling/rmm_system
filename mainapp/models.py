626e9cb49baf30e9b0815d24d48ca46ad8ca8459
vladimir <korovka11sving@gmail.com>
2021-03-26 13:16:54 +0500

    changed docker-compose file: added grafana volume

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Machine(models.Model):
-    ip_address = models.GenericIPAddressField(max_length=15, null=False, verbose_name='Ip address')
+    ip_address = models.GenericIPAddressField(null=False, verbose_name='Ip address')
     port = models.IntegerField(null=False, verbose_name='Port')
     machine_name = models.CharField(max_length=100, null=False, verbose_name='Computer name')
     os = models.CharField(max_length=50, null=False, verbose_name='Operating system')
